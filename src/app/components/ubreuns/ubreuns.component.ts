3a295954e7fd6910f947b9753878b8da8a344ef5
developerdesigner18 <developer.designer18@gmail.com>
2022-04-07 18:12:24 +0530

    Changes about-us slider

--- src/app/components/ubreuns/ubreuns.component.ts
+++ src/app/components/ubreuns/ubreuns.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgxPaginationModule } from 'ngx-pagination';
 
 @Component({
   selector: 'app-ubreuns',
@@ -6,8 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ubreuns.component.scss']
 })
 export class UbreunsComponent implements OnInit {
+  image:any[] = ["/assets/img/about-us/Rectangle_98.png",
+    "/assets/img/about-us/Rectangle_98_dg.png",
+    "/assets/img/about-us/Rectangle_98_dg@2x.png",
+    "/assets/img/about-us/Rectangle_98_cy.png",
+    "/assets/img/about-us/Rectangle_98_cy.png",
+    "/assets/img/about-us/Rectangle_98@2x.png"]
+  p: number = 1;
 
-  constructor() { }
+  constructor() {
+    // for (let i = 1; i <= 10; i++) {
+    //   this.collection.push(`item ${i}`);
+    // }
+  }
 
   ngOnInit(): void {
   }
