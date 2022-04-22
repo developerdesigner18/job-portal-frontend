82128dace9ed912d241d8b58f99bc21287a5eb97
Developer Designer <developer.designer18@gmail.com>
2022-04-22 14:52:59 +0530

    style issue solved in imprint page and addeda list component

--- src/app/components/stadt/stadt-routing.module.ts
+++ src/app/components/stadt/stadt-routing.module.ts
@@ -3,7 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { StadtComponent } from './stadt.component';
 
 const routes: Routes = [
-  { path: '', component: StadtComponent},];
+  { path: '', component: StadtComponent},{ path: 'list', loadChildren: () => import('./list/list.module').then(m => m.ListModule) }];
+
+
+
+  // const routes: Routes = [{ path: '', component: AdminComponent },{ path: 'admin-dashboard', loadChildren: () => import('./admin-dashboard/admin-dashboard.module').then(m => m.AdminDashboardModule) },
+  // { path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
