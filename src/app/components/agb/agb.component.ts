c91c7cb5dab62b754d04b3d342ae854b358d7d59
Developer Designer <developer.designer18@gmail.com>
2022-04-30 13:11:53 +0530

    make changes in a agb page

--- src/app/components/agb/agb.component.ts
+++ src/app/components/agb/agb.component.ts
@@ -6,10 +6,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./agb.component.scss']
 })
 export class AgbComponent implements OnInit {
+  management_check: boolean = true;
+  advert_check: boolean;
+  travel_check: boolean;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  checkboxes(box_name: any) {
+    console.log(box_name);
+    if (box_name == 'management_check') {
+      this.management_check = true
+      this.advert_check = false
+      this.travel_check = false
+    } else if (box_name == 'advert_check') {
+      this.management_check = false
+      this.advert_check = true
+      this.travel_check = false
+    } else if (box_name == 'travel_check'){
+      this.management_check = false
+      this.advert_check = false
+      this.travel_check = true
+    }
   }
 
 }
