6bfa179f368f0c755a57bfccd8f80c925ee2411d
Developer Designer <developer.designer18@gmail.com>
2022-05-05 18:26:15 +0530

    favicon changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,37 +2,58 @@ import { Component } from '@angular/core';
 import { ActivatedRoute, Router , NavigationEnd  } from '@angular/router';
 import { Observable } from 'rxjs';
 import {filter} from 'rxjs/operators';
+import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
 
-
+// declare var jquery: any;
+declare var $: any;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
   title = 'jobportal';
   name = 'Get Current Url Route Demo';
   data:any;
-
-  constructor(private actroute:ActivatedRoute, private router: Router) { }
+  closeResult = '';
+  constructor(private actroute:ActivatedRoute, private router: Router , private modalService: NgbModal) { }
   
-  scrollFunc() {
-    // console.log('here');
-    
-    this.actroute.fragment.subscribe((fragment: string) => {
-      // console.log("fragemnet============",fragment)
-      const element = document.getElementById(fragment)
-      // console.log('fragment', fragment, 'element', element);
-      
-      if (fragment && element != null) {
-        element.scrollIntoView();
-      }
+  open(content:any) {
+    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
     });
   }
 
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return `with: ${reason}`;
+    }
+  }
+
+  // scrollFunc() {
+  //   // console.log('here');
+    
+  //   this.actroute.fragment.subscribe((fragment: string) => {
+  //     // console.log("fragemnet============",fragment)
+  //     const element = document.getElementById(fragment)
+  //     console.log('fragment', fragment, 'element', element);
+  //     // $('#sportsPriese').scrollIntoView();
+  //     if (fragment != null && element != null) {
+  //       element.scrollIntoView();
+  //     }
+  //   });
+  // }
+
   ngOnInit(): void { 
-    setTimeout(() => {
-      this.scrollFunc();
-    }, 4000)
+    // setTimeout(() => {
+    //   this.scrollFunc();
+    // }, 4000)
   }
 }
--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
@@ -8,15 +8,54 @@ import { ActivatedRoute, Router } from '@angular/router';
 })
 export class FooterComponent implements OnInit {
 
-  constructor(public router:Router, private actroute:ActivatedRoute) { }
+  constructor(public router:Router, private actroute:ActivatedRoute) { 
 
-  scrollFunc() {
-    let fragment = this.actroute.snapshot.fragment
-    let element = document.getElementById(fragment)
-    element?.scrollIntoView()
+
+
   }
+
+
+  scrollFunc(): void {
+
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+          const tree = this.router.parseUrl(this.router.url);
+          if (tree.fragment) {
+        const element = document.querySelector("#" + tree.fragment);
+        if (element) { element?.scrollIntoView(); }
+          }
+        // let fragment = window.location.hash.slice(1)
+        // let element = document.getElementById(fragment)
+        // console.log(fragment , element)
+        // element?.scrollIntoView()
+       }
+  });
+
+    // let fragment = window.location.hash.slice(1)
+    // let element = document.getElementById(fragment)
+    // console.log(fragment , element)
+    // element?.scrollIntoView()
+  }
+
+  // scrollFunc(fragment: any) {
+  //   document.addEventListener("DOMContentLoaded", (event) => { 
+  //   // let fragment = this.actroute.snapshot.fragment
+  //   let element = document.getElementById(fragment)
+
+  //   console.log(fragment, element);
+    
+  //   element?.scrollIntoView()
+  //   })
+  // }
 
   ngOnInit(): void {
+    // document.addEventListener("DOMContentLoaded", (event) => { 
+      this.scrollFunc()
+    // });    
   }
+
+  // ngOnInit(): void {
+
+  // }
 
 }
