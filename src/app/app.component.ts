899a10d0b14e90b3111860e42bf1b54918105ae2
Developer Designer <developer.designer18@gmail.com>
2022-05-09 11:58:09 +0530

    added a google tag manage and meta properties

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,39 +1,59 @@
 import { Component } from '@angular/core';
-import { ActivatedRoute, Router , NavigationEnd, ResolveEnd  } from '@angular/router';
+import {
+  ActivatedRoute,
+  Router,
+  NavigationEnd,
+  ResolveEnd,
+} from '@angular/router';
 import { Observable } from 'rxjs';
-import {filter} from 'rxjs/operators';
-import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
+import { filter } from 'rxjs/operators';
+import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
+import { Meta, Title } from '@angular/platform-browser';
 
 // declare var jquery: any;
 declare var $: any;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-
 export class AppComponent {
-  title = 'jobportal';
+  title = 'jobportal,sportmarketing,stadtmarketing';
   name = 'Get Current Url Route Demo';
-  data:any;
-  constructor(private actroute:ActivatedRoute, private router: Router , private modalService: NgbModal) {
-
-// To get a clicked routes in a console start
+  data: any;
+  constructor(
+    private actroute: ActivatedRoute,
+    private router: Router,
+    private modalService: NgbModal,
+    private titleService: Title,
+    private meta: Meta
+  ) {
+    // To get a clicked routes in a console start
     this.router.events.subscribe((routerData) => {
-      if(routerData instanceof ResolveEnd){ 
-        console.log(routerData.url)
-         if(routerData.url === 'your url'){
-           //Do something
-         }
-    } 
-})
-// To get a clicked routes in a console end
-    
-   }
-  
+      if (routerData instanceof ResolveEnd) {
+        console.log(routerData.url);
+        if (routerData.url === 'your url') {
+          //Do something
+        }
+      }
+    });
+    // To get a clicked routes in a console end
+  }
 
- 
-  ngOnInit(): void { 
+  ngOnInit(): void {
+    this.titleService.setTitle(this.title);
+    this.meta.addTags([
+      { name: 'keywords', content: 'jobportal,sportmarketing,stadtmarketing' },
+      {
+        name: 'description',
+        content: 'jobportal,sportmarketing,stadtmarketing',
+      },
+      { name: 'robots', content: 'index, follow' },
+    ]);
 
+    // const writer = this.meta.getTag('name=description');
+    // console.log(writer);
+    // this.meta.updateTag({ name: 'description', content: 'Harry Potter' });
+    // this.meta.removeTag('name="writer"');
   }
 }
