5cbdb346261ddfeff7cca0a47785e4eb7cc433f2
Developer Designer <developer.designer18@gmail.com>
2022-05-24 16:14:15 +0530

    added a changes in sports data crud

--- src/app/admin/admin-sports/sports-data/sports-data.component.ts
+++ src/app/admin/admin-sports/sports-data/sports-data.component.ts
@@ -69,6 +69,12 @@ export class SportsDataComponent implements OnInit {
     this.getTravelInfoAll()
   }
 
+  getControlValidation(key: string): boolean {
+    const { invalid, touched, dirty } = this.travelInfoForm.get(key) as FormGroup;
+    // console.log(key);
+    return invalid && (touched || dirty);
+  }
+
   getTravelInfoAll() {
     this.commanservice.gettravelInfoAll().subscribe(
       res => {
@@ -89,8 +95,7 @@ export class SportsDataComponent implements OnInit {
   res => {
     Notiflix.Loading.remove();
     this.travelInfoById = res.data
-    console.log('this.travelInfoById', this.travelInfoById.cover_image_travel);
-    
+
     this.travelInfoForm.patchValue(res.data)
     console.log('###boatInfoById', this.travelInfoById);
     if (!res.success) { Notiflix.Notify.failure(res.error); }
@@ -101,6 +106,45 @@ export class SportsDataComponent implements OnInit {
 );
 }
 
+insertTravelInfo(){
+  this.travelInfoForm.reset()
+  this.travelInfoById = []
+  this.removeCoverImage()
+  this.remove_image_all()
+  this.show_form = true;
+
+}
+
+ uppdatetravelinfo(datas:any){
+  this.show_form = true;
+  Notiflix.Loading.standard({
+    cssAnimationDuration: 2000,
+    backgroundColor: '0, 0, 0, 0.0',
+  },
+  )
+  this.getTravelinfobyid(datas)
+}
+
+
+// deleteBoatImage(boat_id: any, image_id: any) {
+//   Notiflix.Loading.standard({
+//     cssAnimationDuration: 2000,
+//     backgroundColor: '0, 0, 0, 0.0',
+//   },
+//   )
+//   this.commanservice.deleteBoatImage(boat_id, image_id).subscribe(
+//     res => {
+//       Notiflix.Loading.remove();
+//       Notiflix.Notify.success(res.body.message);        
+//       this.getBoatInfoById(boat_id)
+//       if (!res.body.success) { Notiflix.Notify.failure(res.error); }
+//     },
+//     err => {
+//       Notiflix.Loading.remove();
+//       Notiflix.Notify.failure(err.error?.message);
+//     }
+//   )
+// }
 
 deleteTravelInfo(boat_id: any) {
   Notiflix.Loading.standard({
@@ -126,51 +170,49 @@ deleteTravelInfo(boat_id: any) {
   )
 }
 
-  getControlValidation(key: string): boolean {
-    const { invalid, touched, dirty } = this.travelInfoForm.get(key) as FormGroup;
-    // console.log(key);
-    return invalid && (touched || dirty);
-  }
-
-  onCoverImageUpload(event: any) {
-    Notiflix.Loading.standard({
-      cssAnimationDuration: 2000,
-      backgroundColor: '0, 0, 0, 0.0',
-    },
-    )
-    const file = event.target.files && event.target.files[0];
-    if (file) {
-      var reader = new FileReader();
-      reader.readAsDataURL(file);
-      if(file.type.indexOf('image')> -1){
-        this.cover_image_travel = file
-        reader.onload = (event) => {
-          this.cover_image_url = (<FileReader>event.target).result;
-        }
+onCoverImageUpload(event: any) {
+  Notiflix.Loading.standard({
+    cssAnimationDuration: 2000,
+    backgroundColor: '0, 0, 0, 0.0',
+  },
+  )
+  const file = event.target.files && event.target.files[0];
+  if (file) {
+    var reader = new FileReader();
+    reader.readAsDataURL(file);
+    if(file.type.indexOf('image')> -1){
+      this.cover_image_travel = file
+      reader.onload = (event) => {
+        this.cover_image_url = (<FileReader>event.target).result;
       }
     }
-    Notiflix.Loading.remove();
   }
+  Notiflix.Loading.remove();
+}
 
-  onBoatImagesUpload(event: any) {
-    Notiflix.Loading.standard({
-      cssAnimationDuration: 2000,
-      backgroundColor: '0, 0, 0, 0.0',
-    },
-    )
-    for (let index = 0; index < event.target.files.length; index++) {
-      const file = event.target.files[index];
-      var reader = new FileReader();
-      reader.readAsDataURL(file);
-      if(file.type.indexOf('image')> -1){        
-        this.travel_images.push(file)
-        reader.onload = (event) => {
-          this.travel_images_url.push((<FileReader>event.target).result);
-        }
+onBoatImagesUpload(event: any) {
+  Notiflix.Loading.standard({
+    cssAnimationDuration: 2000,
+    backgroundColor: '0, 0, 0, 0.0',
+  },
+  )
+  for (let index = 0; index < event.target.files.length; index++) {
+    const file = event.target.files[index];
+    var reader = new FileReader();
+    reader.readAsDataURL(file);
+    if(file.type.indexOf('image')> -1){        
+      this.travel_images.push(file)
+      reader.onload = (event) => {
+        this.travel_images_url.push((<FileReader>event.target).result);
       }
-    }    
-    Notiflix.Loading.remove();
-  }
+    }
+  }    
+  Notiflix.Loading.remove();
+}
+
+ 
+
+
 
   removeCoverImage() {
     this.cover_image_url = undefined
@@ -196,8 +238,48 @@ deleteTravelInfo(boat_id: any) {
   }
 
 
-  insertTravelInfo(){
-    this.show_form = true;
+ async submitdata(){
+  if (this.travelInfoForm.value._id != null) {
+    // console.log("called update") 
+
+    if (this.travelInfoForm.pristine && !this.cover_image_travel ) {
+      return;
+    }
+    const { value } = this.travelInfoForm;
+    value.cover_image_travel = this.cover_image_travel
+
+
+    // console.log(value);
+    var data = {
+      user:this.userdata
+    }
+    var fd = new FormData();
+    fd.append('name', value.travel_info.name);
+    fd.append('description', value.travel_info.description);
+    fd.append('Url', value.travel_info.Url);
+    fd.append('cover_image_travel', value.cover_image_travel);
+    Notiflix.Loading.standard({
+      cssAnimationDuration: 2000,
+      backgroundColor: '0, 0, 0, 0.0',
+    },
+    )
+    this.commanservice.updateTravelInfo(this.travelInfoForm.value._id, fd ,data).subscribe(
+      res => {
+        Notiflix.Loading.remove();
+        Notiflix.Notify.success(res.body.message);
+        this.getTravelinfobyid(this.travelInfoForm.value._id)
+        // this.remove_image_all()
+        if (!res.body.success) { Notiflix.Notify.failure(res.body.error); }
+      },
+      err => {
+        Notiflix.Loading.remove();
+        Notiflix.Notify.failure(err.error?.message);
+      }
+    )
+ 
+  }
+  else{
+    console.log("called insert") 
 
     if (this.travelInfoForm.pristine && !this.cover_image_travel) {
       return;
@@ -206,7 +288,7 @@ deleteTravelInfo(boat_id: any) {
     const { value } = this.travelInfoForm;
     value.cover_image_travel = this.cover_image_travel
 
-    console.log(value);
+    // console.log(value);
 
     var fd = new FormData();
     fd.append('name', value.travel_info.name);
@@ -226,7 +308,7 @@ deleteTravelInfo(boat_id: any) {
 
     this.commanservice.inserttravelInfo(fd , data).subscribe(
       res => {
-        console.log("response",res)
+        // console.log("response",res)
         Notiflix.Loading.remove();
         Notiflix.Notify.success(res.body.message);
         this.getTravelInfoAll()
@@ -239,58 +321,7 @@ deleteTravelInfo(boat_id: any) {
       }
     )
   }
+ }
 
-
-  async uppdatetravelinfo(datas:any){
-    this.show_form  =  true
-    Notiflix.Loading.standard({
-      cssAnimationDuration: 2000,
-      backgroundColor: '0, 0, 0, 0.0',
-    },
-    )
-    this.getTravelinfobyid(datas)
-
-    if (this.travelInfoForm.value._id != null) {
-      if (this.travelInfoForm.pristine && !this.cover_image_travel ) {
-        return;
-      }
-  
-      const { value } = this.travelInfoForm;
-      value.cover_image_travel = this.cover_image_travel
-
-  
-      console.log(value);
-  
-      var fd = new FormData();
-      fd.append('name', value.travel_info.name);
-      fd.append('description', value.travel_info.description);
-      fd.append('Url', value.travel_info.Url);
-      fd.append('cover_image_travel', value.cover_image_travel);
-  
-      var data = {
-        user:this.userdata
-      }
-
-      Notiflix.Loading.standard({
-        cssAnimationDuration: 2000,
-        backgroundColor: '0, 0, 0, 0.0',
-      },
-      )
-
-      this.commanservice.updateTravelInfo(datas._id, fd ,data).subscribe(
-        res => {
-          Notiflix.Loading.remove();
-          Notiflix.Notify.success(res.body.message);
-          this.getTravelinfobyid(this.travelInfoForm.value._id)
-          // this.remove_image_all()
-          if (!res.body.success) { Notiflix.Notify.failure(res.body.error); }
-        },
-        err => {
-          Notiflix.Loading.remove();
-          Notiflix.Notify.failure(err.error?.message);
-        }
-      )
-  }
-}
 
 }
